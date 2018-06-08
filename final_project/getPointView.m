a1dd720a722f42633480e2b420bf925e440e2039
Michaelito170 <38978290+Michaelito170@users.noreply.github.com>
2018-06-08 11:10:19 +0200

    split off xy

--- final_project/getPointView.m
+++ final_project/getPointView.m
@@ -64,22 +64,7 @@ for j = 1:length(matches_prev_image)
         end
 end
 end
-%% get actual point coordinates from PT matrix
-XY_PT_matrix=[];
-for i = 1:19
-    for j =1:length(PT_matrix)
-        points= frames{1,i};
-        if PT_matrix(i,j) ~= 0
-            
-           XY_PT_matrix(i+i-1:i+i,j)= points(1:2,PT_matrix(i,j));
-           
-    else 
-        
-XY_PT_matrix(i+i-1:i+i,j)= [0;0];
 
-    end
-    end
-end
 end
         
         
